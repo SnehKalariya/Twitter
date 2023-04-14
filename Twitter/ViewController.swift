901aa4b0fba569cd1687a224b544c826875759fb
SnehKalariya <snehkalariya9583@gmail.com>
2023-04-14 10:56:21 +0530

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Sneh kalariya on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
